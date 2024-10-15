982f0a255ae9c7e5ea3a1868e0cb30ffaab420d5
Callum <callumtaitmate@gmail.com>
2024-10-15 01:31:53 +0100

    Header Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+'use client';
+import { SignedIn, UserButton } from '@clerk/nextjs'
 import { Button } from './ui/button'
 import Link from 'next/link'
 import { FilePlus2 } from 'lucide-react'
 import UpgradeButton from './UpgradeButton'
+import useSubscription from "@/hooks/useSubscription";
 
 function Header() {
+    const { hasActiveMembership } = useSubscription();
     return (
         <div className='flex justify-between bg-white shadow-sm p-5 border-b'>
             <Link href="/dashboard"
@@ -17,9 +20,11 @@ function Header() {
             <SignedIn>
                 <div className='flex items-center space-x-2'>
 
-                    <Button asChild variant="link" className='hidden md:flex'>
-                        <Link href="/dashboard/upgrade">Pricing</Link>
-                    </Button>
+                    {!hasActiveMembership &&
+                        <Button asChild variant="link" className='hidden md:flex'>
+                            <Link href="/dashboard/upgrade">Pricing</Link>
+                        </Button>
+                    }
 
                     <Button asChild variant="outline" className=''>
                         <Link href="/dashboard">My Documents</Link>
