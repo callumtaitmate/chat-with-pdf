e136c91ef525882eb20ad85d7075e91397def9d1
Callum <callumtaitmate@gmail.com>
2024-11-06 17:41:26 +0000

    Updated Sign-Up Flow

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,8 @@
+import { SignUp } from '@clerk/nextjs'
+
+export default function Page() {
+  return <div className='w-full flex justify-center mt-10'>
+    <SignUp forceRedirectUrl="/dashboard"
+    />
+  </div>
+}
